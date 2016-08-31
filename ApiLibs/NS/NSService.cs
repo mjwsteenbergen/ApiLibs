30ae891d733875c5fe872b968cf890a5e285a863
newnottakenname <martijnsreclame@hotmail.nl>
2016-08-31 12:53:06 +0200

    Start on NSService

---
+++ ApiLibs/NS/NSService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiLibs.NS
+{
+    class NSService : Service
+    {
+
+        public NSService(string username, string password)
+        {
+            AddStandardHeader(new Param("CURLOPT_USERPWD", username + ":" + password));
+        }
+    }
+}
