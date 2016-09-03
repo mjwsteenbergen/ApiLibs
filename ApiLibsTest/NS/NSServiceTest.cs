e600a345cfd10af6e8db98367bfed08d2502482f
newnottakenname <martijnsreclame@hotmail.nl>
2016-09-03 12:57:45 +0200

    Continuation

--- ApiLibsTest/NS/NSServiceTest.cs
+++ ApiLibsTest/NS/NSServiceTest.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
 
 namespace ApiLibsTest.NS
 {
+    [TestFixture]
     class NSServiceTest
     {
+        public void Test()
+        {
+            
+        }
     }
 }
