0afcd51e1ecdbb6d73135cc5d0a15019a7b63f77
mjwsteenbergen <martijnsreclame@hotmail.nl>
2017-03-02 17:01:47 +0100

    Update Instapaper to version 1.1

--- ApiLibs/Instapaper/ReturnClasses.cs
+++ ApiLibs/Instapaper/ReturnClasses.cs
@@ -6,22 +6,6 @@ using System.Threading.Tasks;
 
 namespace ApiLibs.Instapaper
 {
-    public class Bookmark
-    {
-        public string type { get; set; }
-        public string hash { get; set; }
-        public string description { get; set; }
-        public int bookmark_id { get; set; }
-        public string private_source { get; set; }
-        public string title { get; set; }
-        public string url { get; set; }
-        public int progress_timestamp { get; set; }
-        public double time { get; set; }
-        public float progress { get; set; }
-        public string starred { get; set; }
-    }
-
-
     public class Folder
     {
         public string title { get; set; }
@@ -32,6 +16,48 @@ namespace ApiLibs.Instapaper
         public string type { get; set; }
         public string slug { get; set; }
     }
+
+
+    public class BookmarksObject
+    {
+        public List<Highlight> highlights { get; set; }
+        public List<Bookmark> bookmarks { get; set; }
+        public User user { get; set; }
+    }
+
+    public class User
+    {
+        public string username { get; set; }
+        public int user_id { get; set; }
+        public string type { get; set; }
+        public string subscription_is_active { get; set; }
+    }
+
+    public class Highlight
+    {
+        public int highlight_id { get; set; }
+        public string text { get; set; }
+        public int bookmark_id { get; set; }
+        public int time { get; set; }
+        public int position { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Bookmark
+    {
+        public string hash { get; set; }
+        public string description { get; set; }
+        public int bookmark_id { get; set; }
+        public string private_source { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public int progress_timestamp { get; set; }
+        public int time { get; set; }
+        public float progress { get; set; }
+        public string starred { get; set; }
+        public string type { get; set; }
+    }
+
 
 
 }
