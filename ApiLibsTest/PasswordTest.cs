dd406f12439a846d359a6e21283d319e027b0755
Martijn Steenbergen <martijnsreclame@hotmail.nl>
2018-03-17 16:21:18 +0100

    Better password management

---
+++ ApiLibsTest/PasswordTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ApiLibs.General;
+using System.IO;
+
+namespace ApiLibsTest
+{
+    public class PasswordTest 
+    {
+        public static readonly string GlobalAppDataPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + Path.DirectorySeparatorChar +
+            "Laurentia" + Path.DirectorySeparatorChar;
+
+        [Test]
+        public void GetPasswords()
+        {
+            var p = Passwords.ReadPasswords(GlobalAppDataPath);
+            Assert.NotNull(p.Telegram_token);
+        }
+    }
+}
