36f6adf787f702b50560173aacf7c03eebdd2b5e
Martijn Steenbergen <m.uses+git@outlook.com>
2018-09-08 15:07:22 +0200

    Remove old tests

---
+++ ApiLibsTest/Todoist/TodoistServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ApiLibs.General;
+using ApiLibs.MicrosoftGraph;
+using ApiLibs.Todoist;
+using NUnit.Framework;
+
+namespace ApiLibsTest.Todoist
+{
+    class TodoistServiceTest
+    {
+        private TodoistService todoistService;
+
+        [OneTimeSetUp]
+        public void GetTodoistService()
+        {
+            Passwords passwords = Passwords.ReadPasswords();
+            todoistService = new TodoistService(passwords.TodoistKey, passwords.TodoistUserAgent);
+        }
+
+        [Test]
+        public async Task SyncTest()
+        {
+            await todoistService.GetLabels();
+        }
+    }
+}
