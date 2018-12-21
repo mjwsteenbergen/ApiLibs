a84186828bf9e27abbe2b6b989e49a7723308397
Martijn Steenbergen <m.uses+git@outlook.com>
2018-12-21 12:04:33 +0100

    Add feedly api

---
+++ ApiLibs/Feedly/FeedlyService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiLibs.Feedly
+{
+    public class FeedlyService : Service
+    {
+        public FeedlyService() : base("https://cloud.feedly.com") { }
+
+        private string UserId;
+
+        public FeedlyService(string token) : base("https://cloud.feedly.com/v3/")
+        {
+            AddStandardHeader("Authorization", $"OAuth ${token}");
+        }
+
+        public async Task<Stream> GetSavedArticles()
+        {
+            await GetUserId();
+            return await MakeRequest<Stream>($"streams/contents?streamId=user/{UserId}/tag/global.saved");
+        }
+
+        private async Task GetUserId()
+        {
+            if (string.IsNullOrEmpty(UserId))
+            {
+                UserId = (await GetUser()).Id;
+            }
+        }
+
+        public Task<Profile> GetUser()
+        {
+            return MakeRequest<Profile>("profile");
+        }
+
+        public Task MarkAsUnsaved(Article a)
+        {
+            return MarkAsUnsaved(a.Id);
+        }
+
+        public Task MarkAsUnsaved(string id)
+        {
+            return HandleRequest("/markers", Call.POST, content: new
+            {
+                action = "markAsUnsaved",
+                type = "entries",
+                entryIds = new[] {
+                    id
+                }
+            });
+        }
+
+        public Task MarkAsSaved(Article a)
+        {
+            return MarkAsSaved(a.Id);
+        }
+
+        public Task MarkAsSaved(string id)
+        {
+            return HandleRequest("/markers", Call.POST, content: new
+            {
+                action = "markAsSaved",
+                type = "entries",
+                entryIds = new[] {
+                    id
+                }
+            });
+        }
+
+        public Task<Stream> GetItemsFromFeed(string feedId)
+        {
+            return MakeRequest<Stream>($"streams/contents?streamId={feedId}");
+        }
+    }
+}
---
+++ ApiLibsTest/Feedly/FeedlyServiceTest.cs
@@ -0,0 +1,55 @@
+﻿using ApiLibs.Feedly;
+using ApiLibs.General;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiLibsTest.Feedly
+{
+    class FeedlyServiceTest
+    {
+        private FeedlyService feedly;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            Passwords passwords = Passwords.ReadPasswords();
+            feedly = new FeedlyService(passwords.GetPasssword("FeedlyToken"));
+        }
+
+        [Test]
+        public async Task GetSavedTestAsync()
+        {
+            var res = await feedly.GetSavedArticles();
+        }
+
+        [Test]
+        public async Task GetUserTestAsync()
+        {
+            var res = await feedly.GetUser();
+        }
+
+        [Test]
+        public async Task GetArticlesFromFeedTest()
+        {
+            var res = await feedly.GetItemsFromFeed("feed%2Fhttps%3A%2F%2Fcommons.wikimedia.org%2Fw%2Fapi.php%3Faction%3Dfeaturedfeed%26feed%3Dpotd%26feedformat%3Drss%26language%3Den");
+        }
+
+        [Test]
+        public async Task SaveTestAsync()
+        {
+            var wikiStream = await feedly.GetItemsFromFeed("feed%2Fhttps%3A%2F%2Fcommons.wikimedia.org%2Fw%2Fapi.php%3Faction%3Dfeaturedfeed%26feed%3Dpotd%26feedformat%3Drss%26language%3Den");
+            await feedly.MarkAsSaved(wikiStream.Items.First());
+        }
+
+        [Test]
+        public async Task UnSaveTestAsync()
+        {
+            var wikiStream = await feedly.GetItemsFromFeed("feed%2Fhttps%3A%2F%2Fcommons.wikimedia.org%2Fw%2Fapi.php%3Faction%3Dfeaturedfeed%26feed%3Dpotd%26feedformat%3Drss%26language%3Den");
+            await feedly.MarkAsUnsaved(wikiStream.Items.First());
+        }
+    }
+}
