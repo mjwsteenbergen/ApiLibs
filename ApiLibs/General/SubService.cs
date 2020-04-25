82c102e8ffe8d621f2ce4c454be93fdfae606013
Martijn Steenbergen <m.uses+git@outlook.com>
2020-04-25 23:07:57 +0200

    Create generic versions of existing helper classes

--- ApiLibs/General/SubService.cs
+++ ApiLibs/General/SubService.cs
@@ -26,4 +26,23 @@ namespace ApiLibs.General
             return await Service.HandleRequest(url, m, parameters, header, content, statusCode);
         }
     }
+
+    public class SubService<T> where T : Service
+    {
+        protected T Service { get; }
+        public SubService(T service)
+        {
+            Service = service;
+        }
+
+        protected virtual async Task<S> MakeRequest<S>(string url, Call m = Call.GET, List<Param> parameters = null, List<Param> header = null, object content = null, HttpStatusCode statusCode = HttpStatusCode.OK)
+        {
+            return await Service.MakeRequest<S>(url, m, parameters, header, content, statusCode);
+        }
+
+        protected virtual async Task<string> HandleRequest(string url, Call m = Call.GET, List<Param> parameters = null, List<Param> header = null, object content = null, HttpStatusCode statusCode = HttpStatusCode.OK)
+        {
+            return await Service.HandleRequest(url, m, parameters, header, content, statusCode);
+        }
+    }
 }
