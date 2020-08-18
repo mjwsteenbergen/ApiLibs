409e8723585f4f407220a9dd40a0f6d4ddfad8e5
Martijn Steenbergen <m.uses+git@outlook.com>
2020-08-18 17:55:11 +0200

    Add GetWatchlistMovies method for Trakt

--- ApiLibs/Trakt/SyncService.cs
+++ ApiLibs/Trakt/SyncService.cs
@@ -21,6 +21,12 @@ namespace ApiLibs.Trakt
         public Task AddToWatchlist(SyncRequestObject media) => MakeRequest<string>("/sync/watchlist", Call.POST, content: media);
         public Task RemoveFromWatchlist(SyncRequestObject media) => MakeRequest<string>("/sync/watchlist/remove", Call.POST, content: media);
 
+        public Task<List<WrappedMediaObject>> GetWatchlistMovies(string sort = null) => GetWatchlist<WrappedMediaObject>("movies", sort);
+        public Task<List<WrappedMediaObject>> GetWatchlistEpisode(string sort = null) => GetWatchlist<WrappedMediaObject>("episodes", sort);
+        private Task<List<T>> GetWatchlist<T>(string type, string sort = null) => MakeRequest<List<T>>($"/sync/watchlist/{type}", parameters: new List<Param> {
+            new OParam("sort", sort)
+        });
+
         public class SyncRequestObject {
             public List<MediaRequestObject> movies { get; set; }
             public List<MediaRequestObject> shows { get; set; }
