680cbe9d1c36b66c0286b2c4089c3bd8fd360ee1
Martijn Steenbergen <m.uses+git@outlook.com>
2020-11-25 14:00:19 +0100

    Add Todoist Rest Service

---
+++ ApiLibs/Todoist.Rest/ReturnClasses.cs
@@ -0,0 +1,277 @@
+using Newtonsoft.Json;
+using ApiLibs.General;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json.Linq;
+using System.Globalization;
+
+namespace ApiLibs.TodoistRest
+{
+    public partial class TodoistProject : ObjectSearcher<TodoistRestService>
+    {
+        [JsonProperty("id")]
+        public long? Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("parent_id")]
+        public long? ParentId { get; set; }
+
+        [JsonProperty("comment_count")]
+        public long? CommentCount { get; set; }
+
+        [JsonProperty("order")]
+        public long? Order { get; set; }
+
+        [JsonProperty("color")]
+        public long? Color { get; set; }
+
+        [JsonProperty("shared")]
+        public bool? Shared { get; set; }
+
+        [JsonProperty("sync_id")]
+        public long? SyncId { get; set; }
+
+        [JsonProperty("favorite")]
+        public bool? Favorite { get; set; }
+
+        [JsonProperty("inbox_project")]
+        public bool? InboxProject { get; set; }
+
+        public Task Delete() => service.DeleteProject(this);
+        public Task Update() => service.UpdateProject(this);
+    }
+
+    public partial class TodoistSection : ObjectSearcher<TodoistRestService>
+    {
+        [JsonProperty("id")]
+        public long? Id { get; set; }
+
+        [JsonProperty("project_id")]
+        public long? ProjectId { get; set; }
+
+        [JsonProperty("order")]
+        public int? Order { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        public Task Delete() => service.DeleteSection(this);
+        public Task Update() => service.UpdateSection(this);
+    }
+
+    public partial class TodoistTask : ObjectSearcher<TodoistRestService>
+    {
+        [JsonProperty("assignee")]
+        public long? Assignee { get; set; }
+
+        [JsonProperty("comment_count")]
+        public long? CommentCount { get; set; }
+
+        [JsonProperty("completed")]
+        public bool? Completed { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        [JsonProperty("due")]
+        public TodoistDueDate Due { get; set; }
+
+        [JsonProperty("id")]
+        public long? Id { get; set; }
+
+        [JsonProperty("label_ids")]
+        public List<int> LabelIds { get; set; }
+
+        [JsonProperty("order")]
+        public int? Order { get; set; }
+
+        [JsonProperty("priority")]
+        public int? Priority { get; set; }
+
+        [JsonProperty("project_id")]
+        public long? ProjectId { get; set; }
+
+        [JsonProperty("section_id")]
+        public long? SectionId { get; set; }
+
+        [JsonProperty("parent_id")]
+        public long? ParentId { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        public Task Update() => this.service.UpdateTasks(this);
+        public Task Delete() => this.service.DeleteTask(this);
+        public Task Close() => this.service.Close(this);
+        public Task Reopen() => this.service.Reopen(this);
+    }
+
+    public class TodoistRequestTask : TodoistTask
+    {
+        [JsonProperty("due_string")]
+        public string DueString { get; set; }
+
+        [JsonProperty("due_date")]
+        public DateTimeOffset? DueDate { get; set; }
+
+        [JsonProperty("due_datetime")]
+        public DateTimeOffset? DueDateTime { get; set; }
+
+        [JsonProperty("due_lang")]
+        public string DueLang { get; set; }
+
+        public TodoistRequestTask(DateTime dateTime)
+        {
+            DueDate = dateTime.Date;
+            // DueString = dateTime.ToString("dd MMM", CultureInfo.InvariantCulture);
+            DueLang = "en";
+
+
+            if (!DueDate.Equals(dateTime))
+            {
+                DueDateTime = dateTime;
+                DueDate = null;
+                // DueString = dateTime.ToString("dd MMM HH:mm", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public TodoistRequestTask() {}
+
+    }
+
+    public partial class TodoistDueDate
+    {
+
+        [JsonProperty("date")]
+        [JsonConverter(typeof(DateConverter))]
+        public DateTimeOffset? Date { get; set; }
+
+        [JsonProperty("recurring")]
+        public bool? Recurring { get; set; }
+
+        [JsonProperty("datetime")]
+        [JsonConverter(typeof(DateTimeConverter))]
+        public DateTimeOffset? Datetime { get; set; }
+
+        [JsonProperty("string")]
+        public string String { get; set; }
+
+        [JsonProperty("timezone")]
+        public string Timezone { get; set; }
+    }
+
+    internal class DateConverter : JsonConverter<DateTimeOffset>
+    {
+        public override bool CanRead => false;
+
+        public override DateTimeOffset ReadJson(JsonReader reader, Type objectType, DateTimeOffset existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, DateTimeOffset value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString("yyyy-MM-dd"));
+        }
+    }
+
+    internal class DateTimeConverter : JsonConverter<DateTimeOffset>
+    {
+        public override bool CanRead => false;
+
+        public override DateTimeOffset ReadJson(JsonReader reader, Type objectType, DateTimeOffset existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, DateTimeOffset value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+        }
+    }
+
+    public partial class TodoistComment
+    {
+        [JsonProperty("attachment")]
+        public TodoistAttachment Attachment { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("posted")]
+        public DateTimeOffset Posted { get; set; }
+
+        [JsonProperty("project_id")]
+        public long ProjectId { get; set; }
+
+        [JsonProperty("task_id")]
+        public long TaskId { get; set; }
+    }
+
+    public partial class TodoistAttachment
+    {
+        [JsonProperty("file_name")]
+        public string FileName { get; set; }
+
+        [JsonProperty("file_type")]
+        public string FileType { get; set; }
+
+        [JsonProperty("file_url")]
+        public Uri FileUrl { get; set; }
+
+        [JsonProperty("resource_type")]
+        public string ResourceType { get; set; }
+    }
+
+    public partial class TodoistLabel
+    {
+        [JsonProperty("id")]
+        public int? Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("color")]
+        public int? Color { get; set; }
+
+        [JsonProperty("order")]
+        public int? Order { get; set; }
+
+        [JsonProperty("favorite")]
+        public bool? Favorite { get; set; }
+    }
+
+    public static class TodoistColor
+    {
+        public static string ToHex(int color) => color switch
+        {
+            30 => "#b8255f",
+            31 => "#db4035",
+            32 => "#ff9933",
+            33 => "#fad000",
+            34 => "#afb83b",
+            35 => "#7ecc49",
+            36 => "#299438",
+            37 => "#6accbc",
+            38 => "#158fad",
+            39 => "#14aaf5",
+            40 => "#96c3eb",
+            41 => "#4073ff",
+            42 => "#884dff",
+            43 => "#af38eb",
+            44 => "#eb96eb",
+            45 => "#e05194",
+            46 => "#ff8d85",
+            47 => "#808080",
+            48 => "#b8b8b8",
+            49 => "#ccac93",
+            _ => throw new KeyNotFoundException("The following color does not exist in todoist: " + color)
+        };
+    }
+}
---
+++ ApiLibs/Todoist.Rest/TodoistRestService.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLibs.TodoistRest
+{
+    public class TodoistRestService : Service
+    {
+        public TodoistRestService(string token) : base("https://api.todoist.com/rest/v1/", false)
+        {
+            AddStandardHeader("Authorization", $"Bearer {token}");
+        }
+
+        private List<TodoistProject> project_cache = new List<TodoistProject>();
+
+        public Task<List<TodoistProject>> GetProjects() => MakeRequest<List<TodoistProject>>("projects");
+        public Task<TodoistProject> GetProject(int id) => MakeRequest<TodoistProject>("projects/" + id);
+
+        public Task UpdateProject(TodoistProject project) => UpdateProject(project.Id ?? throw new ArgumentNullException(nameof(project.Id)), project);
+        public Task UpdateProject(long id, TodoistProject project) => MakeRequest<TodoistProject>("projects/" + id, Call.POST, content: project, statusCode: System.Net.HttpStatusCode.NoContent);
+
+        public Task DeleteProject(TodoistProject project) => DeleteProject(project.Id ?? throw new ArgumentNullException(nameof(project.Id)));
+        public Task DeleteProject(long id) => MakeRequest<string>("projects/" + id, Call.DELETE);
+
+        public Task<TodoistProject> CreateProject(TodoistProject project) => MakeRequest<TodoistProject>("projects", Call.POST, content: project);
+
+
+        public Task<List<TodoistSection>> GetSections() => MakeRequest<List<TodoistSection>>("sections");
+        public Task<List<TodoistSection>> GetSections(TodoistProject project) => GetSections(project.Id ?? throw new ArgumentNullException(nameof(project.Id)));
+        public Task<List<TodoistSection>> GetSections(long id) => MakeRequest<List<TodoistSection>>("sections", parameters: new List<Param> { new Param("project_id", id) });
+        public Task<TodoistSection> GetSection(long id) => MakeRequest<TodoistSection>("sections/" + id);
+        public Task<TodoistSection> CreateSection(TodoistSection section) => MakeRequest<TodoistSection>("sections", Call.POST, content: section);
+
+        public async Task<TodoistProject> GetProject(string name)
+        {
+            TodoistProject proj = project_cache.FirstOrDefault(i => i.Name == name);
+            if(proj != null) return proj;
+
+            project_cache = await GetProjects();
+            return project_cache.FirstOrDefault(i => i.Name == name) ?? throw new KeyNotFoundException("A project with the following name does not exist: " + name);
+        }
+
+        public Task UpdateSection(long id, TodoistSection section) => MakeRequest<TodoistSection>("sections/" + id, Call.POST, content: section, statusCode: System.Net.HttpStatusCode.NoContent);
+        public Task UpdateSection(TodoistSection section) => UpdateSection(section.Id ?? throw new ArgumentNullException(nameof(section.Id)), section);
+        public Task DeleteSection(long id) => MakeRequest<string>("sections/" + id, Call.DELETE, statusCode: System.Net.HttpStatusCode.NoContent);
+        public Task DeleteSection(TodoistSection section) => DeleteSection(section.Id ?? throw new ArgumentNullException(nameof(section.Id)));
+
+
+        public Task<List<TodoistTask>> GetTasks(long? project_id = null, long? label_id = null, string filter = null, string lang = null, List<long> ids = null) => MakeRequest<List<TodoistTask>>("tasks", parameters: new List<Param> {
+            new OParam(nameof(project_id), project_id),
+            new OParam(nameof(label_id), label_id),
+            new OParam(nameof(filter), filter),
+            new OParam(nameof(lang), lang),
+            new OParam(nameof(ids), ids)
+        });
+        public Task<List<TodoistTask>> GetTasks(TodoistProject proj) => GetTasks(project_id: proj.Id);
+
+        public Task<TodoistTask> GetTask(long id) => MakeRequest<TodoistTask>("tasks/"+id);
+        public Task<TodoistTask> CreateTask(TodoistRequestTask task) => MakeRequest<TodoistTask>("tasks", Call.POST, content: task);
+        public Task UpdateTasks(long id, TodoistTask task) => MakeRequest<string>("tasks", Call.POST, content: task, statusCode: System.Net.HttpStatusCode.NoContent);
+
+
+        public Task UpdateTasks(TodoistTask task) => UpdateTasks(task.Id ?? throw new ArgumentNullException(nameof(task.Id)), task);
+        public Task Close(TodoistTask task) => Close(task.Id ?? throw new ArgumentNullException(nameof(task.Id)));
+        public Task Close(long id) => MakeRequest<string>($"tasks/{id}/close", Call.POST, statusCode: System.Net.HttpStatusCode.NoContent);
+        public Task Reopen(TodoistTask task) => Reopen(task.Id ?? throw new ArgumentNullException(nameof(task.Id)));
+        public Task Reopen(long id) => MakeRequest<string>($"tasks/{id}/reopen", Call.POST, statusCode: System.Net.HttpStatusCode.NoContent);
+        public Task DeleteTask(TodoistTask task) => DeleteTask(task.Id ?? throw new ArgumentNullException(nameof(task.Id)));
+        public Task DeleteTask(long id) => MakeRequest<string>($"tasks/{id}", Call.DELETE, statusCode: System.Net.HttpStatusCode.NoContent);
+
+        public Task<List<TodoistLabel>> GetLabels() => MakeRequest<List<TodoistLabel>>("labels");
+        public Task<TodoistLabel> CreateLabel(TodoistLabel label) => MakeRequest<TodoistLabel>("labels", Call.POST, content: label);
+        public Task UpdateLabel(TodoistLabel label) => UpdateLabel(label.Id ?? throw new ArgumentNullException(nameof(label.Id)), label);
+        public Task UpdateLabel(int id, TodoistLabel label) => MakeRequest<string>("labels/" + id, Call.POST, content: label, statusCode: System.Net.HttpStatusCode.NoContent);
+        public Task DeleteLabel(TodoistLabel label) => DeleteLabel(label.Id ?? throw new ArgumentNullException(nameof(label.Id)));
+        public Task DeleteLabel(int id) => MakeRequest<string>("labels/" + id, Call.DELETE, statusCode: System.Net.HttpStatusCode.NoContent);
+    }
+}
